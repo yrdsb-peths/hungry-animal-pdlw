6a0283a3a7883ad2cefc99a8910db90ca6867ceb
pdlw <166637497+pdlw@users.noreply.github.com>
2024-05-08 13:08:47 -0400

    added apple respawn
    instead of apple permanently being removed when colliding with the elephant, it will respawn in a random position.

--- Elephant.java
+++ Elephant.java
@@ -22,6 +22,16 @@ public class Elephant extends Actor
         {
             move(1);
         }
-        removeTouching(Apple.class);
+        eat();
+    }
+    
+    public void eat()
+    {
+        if(isTouching(Apple.class))
+        {
+            removeTouching(Apple.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.createApple();
+        }
     }
 }
